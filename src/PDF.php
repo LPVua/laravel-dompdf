a68122674d255d2f20d713fc331982b3f226d80e
paul <paullmpas@gmail.com>
2016-05-04 18:40:38 +0300

    fixed setPaper method

--- src/PDF.php
+++ src/PDF.php
@@ -17,251 +17,253 @@ use Illuminate\Http\Response;
 class PDF
 {
 
-    /** @var Dompdf */
-    protected $dompdf;
+	/** @var Dompdf */
+	protected $dompdf;
 
-    /** @var \Illuminate\Contracts\Config\Repository */
-    protected $config;
+	/** @var \Illuminate\Contracts\Config\Repository */
+	protected $config;
 
-    /** @var \Illuminate\Filesystem\Filesystem */
-    protected $files;
+	/** @var \Illuminate\Filesystem\Filesystem */
+	protected $files;
 
-    /** @var \Illuminate\Contracts\View\Factory */
-    protected $view;
+	/** @var \Illuminate\Contracts\View\Factory */
+	protected $view;
 
-    protected $rendered = false;
-    protected $orientation;
-    protected $paper;
-    protected $showWarnings;
-    protected $public_path;
+	protected $rendered = false;
+	protected $orientation;
+	protected $paper;
+	protected $showWarnings;
+	protected $public_path;
 
-    /**
-     * @param Dompdf                                  $dompdf
-     * @param \Illuminate\Contracts\Config\Repository $config
-     * @param \Illuminate\Filesystem\Filesystem       $files
-     * @param \Illuminate\View\Factory                $view
-     */
-    public function __construct(Dompdf $dompdf, ConfigRepository $config, Filesystem $files, ViewFactory $view)
-    {
+	/**
+	 * @param Dompdf                                  $dompdf
+	 * @param \Illuminate\Contracts\Config\Repository $config
+	 * @param \Illuminate\Filesystem\Filesystem       $files
+	 * @param \Illuminate\View\Factory                $view
+	 */
+	public function __construct(Dompdf $dompdf, ConfigRepository $config, Filesystem $files, ViewFactory $view)
+	{
 
-        $this->dompdf = $dompdf;
-        $this->config = $config;
-        $this->files = $files;
-        $this->view = $view;
+		$this->dompdf = $dompdf;
+		$this->config = $config;
+		$this->files = $files;
+		$this->view = $view;
 
-        $this->showWarnings = $this->config->get('dompdf.show_warnings', false);
-    }
+		$this->showWarnings = $this->config->get('dompdf.show_warnings', false);
+	}
 
-    /**
-     * Get the DomPDF instance
-     *
-     * @return Dompdf
-     */
-    public function getDomPDF()
-    {
+	/**
+	 * Get the DomPDF instance
+	 *
+	 * @return Dompdf
+	 */
+	public function getDomPDF()
+	{
 
-        return $this->dompdf;
-    }
+		return $this->dompdf;
+	}
 
-    /**
-     * Set the paper size (default A4)
-     *
-     * @param string $paper
-     * @param string $orientation
-     *
-     * @return $this
-     */
-    public function setPaper($paper, $orientation = 'portrait')
-    {
+	/**
+	 * Set the paper size (default A4)
+	 *
+	 * @param string $paper
+	 * @param string $orientation
+	 *
+	 * @return $this
+	 */
+	public function setPaper($paper, $orientation = 'portrait')
+	{
 
-        $this->dompdf->setPaper($paper)->setOrientation($orientation);
+		$this->dompdf->setPaper($paper)->setOrientation($orientation);
+		$this->orientation = $orientation;
+		$this->paper = $paper;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Show or hide warnings
-     *
-     * @param bool $warnings
-     *
-     * @return $this
-     */
-    public function setWarnings($warnings)
-    {
+	/**
+	 * Show or hide warnings
+	 *
+	 * @param bool $warnings
+	 *
+	 * @return $this
+	 */
+	public function setWarnings($warnings)
+	{
 
-        $this->showWarnings = $warnings;
+		$this->showWarnings = $warnings;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Load a HTML string
-     *
-     * @param string $string
-     * @param string $encoding Not used yet
-     *
-     * @return static
-     */
-    public function loadHTML($string, $encoding = null)
-    {
+	/**
+	 * Load a HTML string
+	 *
+	 * @param string $string
+	 * @param string $encoding Not used yet
+	 *
+	 * @return static
+	 */
+	public function loadHTML($string, $encoding = null)
+	{
 
-        $string = $this->convertEntities($string);
-        $this->dompdf->loadHtml($string, $encoding);
-        $this->rendered = false;
+		$string = $this->convertEntities($string);
+		$this->dompdf->loadHtml($string, $encoding);
+		$this->rendered = false;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Load a HTML file
-     *
-     * @param string $file
-     *
-     * @return static
-     */
-    public function loadFile($file)
-    {
+	/**
+	 * Load a HTML file
+	 *
+	 * @param string $file
+	 *
+	 * @return static
+	 */
+	public function loadFile($file)
+	{
 
-        $this->dompdf->loadHtmlFile($file);
-        $this->rendered = false;
+		$this->dompdf->loadHtmlFile($file);
+		$this->rendered = false;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Load a View and convert to HTML
-     *
-     * @param string $view
-     * @param array  $data
-     * @param array  $mergeData
-     * @param string $encoding Not used yet
-     *
-     * @return static
-     */
-    public function loadView($view, $data = [ ], $mergeData = [ ], $encoding = null)
-    {
+	/**
+	 * Load a View and convert to HTML
+	 *
+	 * @param string $view
+	 * @param array  $data
+	 * @param array  $mergeData
+	 * @param string $encoding Not used yet
+	 *
+	 * @return static
+	 */
+	public function loadView($view, $data = [ ], $mergeData = [ ], $encoding = null)
+	{
 
-        $html = $this->view->make($view, $data, $mergeData)->render();
+		$html = $this->view->make($view, $data, $mergeData)->render();
 
-        return $this->loadHTML($html, $encoding);
-    }
+		return $this->loadHTML($html, $encoding);
+	}
 
-    /**
-     * Output the PDF as a string.
-     *
-     * @return string The rendered PDF as string
-     */
-    public function output()
-    {
+	/**
+	 * Output the PDF as a string.
+	 *
+	 * @return string The rendered PDF as string
+	 */
+	public function output()
+	{
 
-        if (!$this->rendered)
-        {
-            $this->render();
-        }
+		if (!$this->rendered)
+		{
+			$this->render();
+		}
 
-        return $this->dompdf->output();
-    }
+		return $this->dompdf->output();
+	}
 
-    /**
-     * Save the PDF to a file
-     *
-     * @param $filename
-     *
-     * @return static
-     */
-    public function save($filename)
-    {
+	/**
+	 * Save the PDF to a file
+	 *
+	 * @param $filename
+	 *
+	 * @return static
+	 */
+	public function save($filename)
+	{
 
-        $this->files->put($filename, $this->output());
+		$this->files->put($filename, $this->output());
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Make the PDF downloadable by the user
-     *
-     * @param string $filename
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function download($filename = 'document.pdf')
-    {
+	/**
+	 * Make the PDF downloadable by the user
+	 *
+	 * @param string $filename
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function download($filename = 'document.pdf')
+	{
 
-        $output = $this->output();
+		$output = $this->output();
 
-        return new Response($output, 200, [
-            'Content-Type'        => 'application/pdf',
-            'Content-Disposition' => 'attachment; filename="' . $filename . '"'
-        ]);
-    }
+		return new Response($output, 200, [
+			'Content-Type'        => 'application/pdf',
+			'Content-Disposition' => 'attachment; filename="' . $filename . '"'
+		]);
+	}
 
-    /**
-     * Return a response with the PDF to show in the browser
-     *
-     * @param string $filename
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function stream($filename = 'document.pdf')
-    {
+	/**
+	 * Return a response with the PDF to show in the browser
+	 *
+	 * @param string $filename
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function stream($filename = 'document.pdf')
+	{
 
-        $output = $this->output();
+		$output = $this->output();
 
-        return new Response($output, 200, [
-            'Content-Type'        => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="' . $filename . '"',
-        ]);
-    }
+		return new Response($output, 200, [
+			'Content-Type'        => 'application/pdf',
+			'Content-Disposition' => 'inline; filename="' . $filename . '"',
+		]);
+	}
 
-    /**
-     * Render the PDF
-     */
-    protected function render()
-    {
+	/**
+	 * Render the PDF
+	 */
+	protected function render()
+	{
 
-        if (!$this->dompdf)
-        {
-            throw new Exception('DOMPDF not created yet');
-        }
+		if (!$this->dompdf)
+		{
+			throw new Exception('DOMPDF not created yet');
+		}
 
-        $this->dompdf->setPaper($this->paper, $this->orientation);
+		$this->dompdf->setPaper($this->paper, $this->orientation);
 
-        $this->dompdf->render();
+		$this->dompdf->render();
 
-        if ($this->showWarnings)
-        {
-            global $_dompdf_warnings;
-            if (count($_dompdf_warnings))
-            {
-                $warnings = '';
-                foreach ($_dompdf_warnings as $msg)
-                {
-                    $warnings .= $msg . "\n";
-                }
-                // $warnings .= $this->dompdf->get_canvas()->get_cpdf()->messages;
-                if (!empty( $warnings ))
-                {
-                    throw new Exception($warnings);
-                }
-            }
-        }
-        $this->rendered = true;
-    }
+		if ($this->showWarnings)
+		{
+			global $_dompdf_warnings;
+			if (count($_dompdf_warnings))
+			{
+				$warnings = '';
+				foreach ($_dompdf_warnings as $msg)
+				{
+					$warnings .= $msg . "\n";
+				}
+				// $warnings .= $this->dompdf->get_canvas()->get_cpdf()->messages;
+				if (!empty( $warnings ))
+				{
+					throw new Exception($warnings);
+				}
+			}
+		}
+		$this->rendered = true;
+	}
 
 
-    protected function convertEntities($subject)
-    {
+	protected function convertEntities($subject)
+	{
 
-        $entities = [
-            '€' => '&#0128;',
-        ];
+		$entities = [
+			'€' => '&#0128;',
+		];
 
-        foreach ($entities as $search => $replace)
-        {
-            $subject = str_replace($search, $replace, $subject);
-        }
+		foreach ($entities as $search => $replace)
+		{
+			$subject = str_replace($search, $replace, $subject);
+		}
 
-        return $subject;
-    }
+		return $subject;
+	}
 
 }
